ac28c7b65f92ab9abc701c98cec7fa800b3cb6bd
Burak Celiloglu <bkceliloglu@gmail.com>
2024-05-29 16:49:53 +0300

    some projects deleted.

--- W01/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace W01.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
--- W03/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace W03.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
